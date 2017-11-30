de6313800cda7b4f4e4109e620809bb460d251cb
Gabriel Robles <garh1505@gmail.com>
2017-11-30 18:53:37 -0500

    Users views and controllers (#4)
    * Users views and controllers
    
    * minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   root 'welcome#index'
-
+  get '/users/login', to: 'users#login'
+  post 'users/login', to: 'users#req'
+  get '/users/login/failure', to: 'users#failure'
+  get '/users/new/success', to: 'users#success'
   resources :seniors
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
