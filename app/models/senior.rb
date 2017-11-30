5fbe1f157548f17ce5599ba731011998d55a9cd1
Jordan Barnartt <jbarnartt@gmail.com>
2017-11-30 23:30:41 +0000

    Simplify name concatentation

--- app/models/senior.rb
+++ app/models/senior.rb
@@ -1,5 +1,5 @@
 class Senior < ApplicationRecord
-  def cat_senior_name(first_name, last_initial)
-    @senior_name_fulL = first_name + " " + last_initial + "."
+  def name
+    first_name + " " + last_initial + "."
   end
 end
