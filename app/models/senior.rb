9447a01beaa9d23b408abdb048c0143ebbfe67e9
Jordan Barnartt <jbarnartt@gmail.com>
2017-12-01 00:24:01 +0000

    Created date and time methods

--- app/models/senior.rb
+++ app/models/senior.rb
@@ -2,4 +2,17 @@ class Senior < ApplicationRecord
   def name
     first_name + " " + last_initial + "."
   end
+  
+  # Date and time formatting
+  def dt_format
+    "%Y-%m-%d %H:%M"
+  end
+  
+  def dt_created
+    created_at.strftime(dt_format)
+  end
+  
+  def dt_updated
+    updated_at.strftime(dt_format)
+  end
 end
